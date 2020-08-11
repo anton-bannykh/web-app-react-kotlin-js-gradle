3db3422d4253920339751ec936732b0ba19f3cc1
Anton Bannykh <anton.bannykh@jetbrains.com>
2020-08-11 10:46:43 +0300

    ~ fix a bug

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,12 @@ import kotlinx.browser.document
 import kotlinx.css.*
 import styled.*
 
-data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
+external interface Video {
+    val id: Int
+    val title: String
+    val speaker: String
+    val videoUrl: String
+}
 
 fun main() {
     render(document.getElementById("root")) {
